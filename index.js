269f013526fb2c153ecf2ab7f9cbb878f868335a
codestarrymilky <1930449882@qq.com>
2024-07-15 12:07:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function drop() {
+    function toggleDropdown() {
       var dropdown = document.getElementById('dropdown').getElementsByTagName('ul')[1];
       dropdown.classList.toggle('show');
-}
+    }
