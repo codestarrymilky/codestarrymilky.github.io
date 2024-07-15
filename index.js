d73c4e342527fb6300db51890744ebcdf26ee212
codestarrymilky <1930449882@qq.com>
2024-07-15 12:27:55 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const dropbar = document.getElementById('dropbar');
 const dropdown = document.getElementById('dropdown');
 
 
-
+// 电脑
 dropbar.addEventListener('mouseover', function () {
   dropdown.style.display = 'block';
 });
 
 dropbar.addEventListener('mouseout', function () {
+      dropdown.style.display = 'none';
+});
+
+// mobile
+dropbar.addEventListener('touchstart', function (e) {
+      e.preventDefault();
+      dropdown.style.display = 'block';
+});
+
+dropbar.addEventListener('touchend', function (e) {
+      e.preventDefault();
       dropdown.style.display = 'none';
 });
