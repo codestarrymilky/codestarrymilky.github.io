5b4c5578c5a1c4fefc38dae5605859e0c04259f9
codestarrymilky <1930449882@qq.com>
2024-10-06 19:29:46 +0800

    Update script.js

--- bianyiqi/script.js
+++ bianyiqi/script.js
@@ -10,20 +10,8 @@
       function generateTemplate() {
         var selectedTemplate = templateSelect.value;
         if (selectedTemplate === "template1") {
-          codeInput.value = "
-                <!DOCTYPE html>
-                <html>
-                    <head>
-                       <title>1</title>
-                       <style>
-
-                       </style>
-                     </head>
-                     <body>
-                
-                     </body>
-               </html>
-          ";
+          codeInput.value = "<!DOCTYPE html>/n<html>/n<head>/n<title>1</title>/n<style>/n/n</style>/n</head>/n<body>/n/n</body>/n</html>";
+        } else if (selectedTemplate === "template2") {";
         } else if (selectedTemplate === "template2") {
           codeInput.value = "<p>模板2内容</p>";
         } else if (selectedTemplate === "template3") {
