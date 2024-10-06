c95239ebb678e4ccee92251527a3137d4768a9d9
codestarrymilky <1930449882@qq.com>
2024-10-07 06:34:03 +0800

    Update script.js

--- bianyiqi/script.js
+++ bianyiqi/script.js
@@ -1,33 +1,28 @@
-      var templateSelect = document.getElementById("templateSelect");
-      var codeInput = document.getElementById("code-input");
-      var previewButton = document.getElementById("preview-button");
-      var codePreview = document.getElementById("code-preview");
+var templateSelect = document.getElementById("templateSelect");
+var codeInput = document.getElementById("code-input");
+var previewButton = document.getElementById("preview-button");
+var codePreview = document.getElementById("code-preview");
 
+function generateTemplate() {
+      var selectedTemplate = templateSelect.value;
 
-
-
-      function generateTemplate() {
-        var selectedTemplate = templateSelect.value;
-
-        if (selectedTemplate === "template1") {
-          codeInput.value = "<!DOCTYPE html>/n<html>/n<head>/n<title>1</title>/n<style>/n/n</style>/n</head>/n<body>/n/n</body>/n</html>";
-        } else if (selectedTemplate === "template2") {
-          codeInput.value = "<h2>模板2内容</h2>";
-        } else if (selectedTemplate === "template3") {
-          codeInput.value = "<p>模板3内容</p>";
-        }
-
-        previewCode();
+      if (selectedTemplate === "template1") {
+         codeInput.value = "<p>模板1内容</p>";
+      } else if (selectedTemplate === "template2") {
+         codeInput.value = "<p>模板2内容</p>";
+      } else if (selectedTemplate === "template3") {
+         codeInput.value = "<p>模板3内容</p>";
       }
 
-      function previewCode() {
-        var code = codeInput.value;
-        codePreview.innerHTML = code;
-      }
+      previewCode();
+}
 
+function previewCode() {
+      var code = codeInput.value;
+      codePreview.innerHTML = code;
+}
 
-      generateTemplate()
-      templateSelect.addEventListener("change", generateTemplate);
-      previewButton.addEventListener("click", previewCode);
-    
+generateTemplate()
 
+templateSelect.addEventListener("change", generateTemplate);
+previewButton.addEventListener("click", previewCode);
